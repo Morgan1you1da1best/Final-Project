ea7ce046fe0e5bf7acca19320b719306dba2cf11
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2018-01-07 02:57:54 -0500

    Updated from Brython Server: 1/7/2018 2:57:53 AM

--- Yahtzee.py
+++ Yahtzee.py
@@ -7,17 +7,18 @@ from ggame import*
 
 
 red = Color(0xFF0000,1)
-green = Color(0x00FF00,1)
+green = Color(0x38AD00,1)
 blue = Color(0x0000FF,1)
 black = Color(0x000000,1)
 white = Color(0xFFFFFF,1)
 
 blackOutline = LineStyle(1,black) #pixels, color
 whiteRectangle = RectangleAsset(100,100,blackOutline,white) #Width, height, outline, fill.
-cardRectangle = RectangleAsset(200,300,blackOutline,white)
+greenRectangle = RectangleAsset(1000,1000,blackOutline,green) #Width, height, outline, fill.
+cardRectangle = RectangleAsset(200,290,blackOutline,white)
 blackCircle = CircleAsset(10,blackOutline,black) #Radius, outline, fill.
 redCircle = CircleAsset(50,blackOutline,red)
-text = TextAsset('Yahtzee' ,fill=red, style='bold 40pt Times')
+text = TextAsset('Yahtzee' ,fill=black, style='bold 40pt Times')
 titleCard = TextAsset('Player 1' ,fill=black, style='10pt Times')
 titleCard2 = TextAsset('Player 2' ,fill=black, style='10pt Times')
 textRoll = TextAsset('Roll' ,fill=black, style='20pt Verdana')
@@ -32,7 +33,9 @@ text4ofakind = TextAsset('4 of a kind:' ,fill=black, style='8pt Times')
 textfullhouse = TextAsset('Full House:' ,fill=black, style='8pt Times')
 textlargestraight = TextAsset('Large Str:' ,fill=black, style='8pt Times')
 textsmallstraight = TextAsset('Small Str:' ,fill=black, style='8pt Times')
+textchance = TextAsset('Chance:' ,fill=black, style='8pt Times')
 textYahtzee = TextAsset('Yahtzee:' ,fill=black, style='8pt Times')
+
 ####################### Die Roll ############################## (dice(dice#)(dot#))
 def numOne(xcord1):
         Sprite(blackCircle,(xcord1,150))
@@ -68,7 +71,7 @@ def numSix(xcord1,xcord2,xcord3,xcord4,xcord5,xcord6):
         Sprite(blackCircle,(xcord6,175))
 
 
-###################################Scoring Functions#######################
+###################################Scoring Functions#1 #######################
 
 def ones():
     count = data['dice'].count(1)
@@ -97,7 +100,8 @@ def sixes():
 def is3ofakind():
     for num in data['dice']:
         count = data['dice'].count(num)
-        if count == 3:
+        if count == 3 or count == 4 or count == 5:
+            data['scoreCard'][6][0] = [sum(data['dice'])]
             print('3ofk:', sum(data['dice']))
             return True
     return False
@@ -105,7 +109,8 @@ def is3ofakind():
 def is4ofakind():
     for num in data['dice']:
         count = data['dice'].count(num)
-        if count == 4:
+        if count == 4 or count == 5:
+            data['scoreCard'][7][0] = [sum(data['dice'])]
             print('4ofk:' ,sum(data['dice']))
             return True
     return False
@@ -115,6 +120,7 @@ def isfullhouse():
     count1 = data['dice'].count(data['dice'][0])
     count2 = data['dice'].count(data['dice'][4])
     if (count1 == 3 and count2 == 2) or (count1 == 2 and count2 == 3):
+        data['scoreCard'][8][0] = [25]
         print('fh:','25')
         return True
     else:
@@ -123,57 +129,154 @@ def isfullhouse():
 def issmallstraight():                              ####smallstraight problem
     data['dice'].sort()
     if 1 in data['dice'] and 2 in data['dice'] and 3 in data['dice'] and 4 in data['dice']:
+        data['scoreCard'][9][0] = [30]
         print('smallstr:','30')
         return True
     elif 2 in data['dice'] and 3 in data['dice'] and 4 in data['dice'] and 5 in data['dice']:
+        data['scoreCard'][9][0] = [30]
         print('smallstr:','30')
         return True
     elif 3 in data['dice'] and 4 in data['dice'] and 5 in data['dice'] and 6 in data['dice']:
+        data['scoreCard'][9][0] = [30]
         print('smallstr:','30')
         return True
     else:
         return False
-
-            
 def islargestraight():                                          
     for num in data['dice']:
         data['dice'].sort()
         if data['dice'] == [1,2,3,4,5]:
+            data['scoreCard'][10][0] = [40]
             print('lgstr:','40')
             return True
         elif data['dice'] == [2,3,4,5,6]:
+            data['scoreCard'][10][0] = [40]
             print('lgstr:','40')
             return True
         else:
             return False
-
 def ischance():
     print('chance:',sum(data['dice']))
+    data['scoreCard'][11][0] = [sum(data['dice'])]
     return True
     
 def isYahtzee():
         for num in data['dice']:
             count = data['dice'].count(num)
             if count == 5:
+                data['scoreCard'][12][0] = [50]
                 print('Yahtezz:','100')
                 return True
             else:
                 return False
-                
+            
+            
+###############################Scoring Function#2 ##############################
+            
+def ones2():
+    count = data['dice'].count(1)
+    data['scoreCard2'][0] = [count]
+
+def twos2():
+    count = data['dice'].count(2)
+    data['scoreCard2'][1][0] = [count]
+    
+def threes2():
+    count = data['dice'].count(3)
+    data['scoreCard2'][2][0] = [count]
+
+def fours2():
+    count = data['dice'].count(4)
+    data['scoreCard2'][3][0] = [count]
+    
+def fives2():
+    count = data['dice'].count(5)
+    data['scoreCard2'][4][0] = [count]
+    
+def sixes2():
+    count = data['dice'].count(6)
+    data['scoreCard2'][5][0] = [count]
+    
+def is3ofakind2():
+    for num in data['dice']:
+        count = data['dice'].count(num)
+        if count == 3 or count == 4 or count == 5:
+            data['scoreCard2'][6][0] = [sum(data['dice'])]
+            print('3ofk:', sum(data['dice']))
+            return True
+    return False
+            
+def is4ofakind2():
+    for num in data['dice']:
+        count = data['dice'].count(num)
+        if count == 4 or count == 5:
+            data['scoreCard2'][7][0] = [sum(data['dice'])]
+            print('4ofk:' ,sum(data['dice']))
+            return True
+    return False
+            
+def isfullhouse2():        
+    data['dice'].sort() 
+    count1 = data['dice'].count(data['dice'][0])
+    count2 = data['dice'].count(data['dice'][4])
+    if (count1 == 3 and count2 == 2) or (count1 == 2 and count2 == 3):
+        data['scoreCard2'][8][0] = [25]
+        print('fh:','25')
+        return True
+    else:
+        return False
+            
+def issmallstraight2():         
+    data['dice'].sort()
+    if 1 in data['dice'] and 2 in data['dice'] and 3 in data['dice'] and 4 in data['dice']:
+        data['scoreCard2'][9][0] = [30]
+        print('smallstr:','30')
+        return True
+    elif 2 in data['dice'] and 3 in data['dice'] and 4 in data['dice'] and 5 in data['dice']:
+        data['scoreCard2'][9][0] = [30]
+        print('smallstr:','30')
+        return True
+    elif 3 in data['dice'] and 4 in data['dice'] and 5 in data['dice'] and 6 in data['dice']:
+        data['scoreCard2'][9][0] = [30]
+        print('smallstr:','30')
+        return True
+    else:
+        return False
+
+            
+def islargestraight2():                                          
+    for num in data['dice']:
+        data['dice'].sort()
+        if data['dice'] == [1,2,3,4,5]:
+            data['scoreCard2'][10][0] = [40]
+            print('lgstr:','40')
+            return True
+        elif data['dice'] == [2,3,4,5,6]:
+            data['scoreCard2'][10][0] = [40]
+            print('lgstr:','40')
+            return True
+        else:
+            return False
+
+def ischance2():
+    print('chance:',sum(data['dice']))
+    data['scoreCard2'][11][0] = [sum(data['dice'])] 
+    return True
+    
+def isYahtzee2():
+        for num in data['dice']:
+            count = data['dice'].count(num)
+            if count == 5:
+                data['scoreCard2'][12][0] = [50]
+                print('Yahtezz:','50')
+                return True
+            else:
+                return False
 ############################End Turn#############################          
 def endTurn():
-    data['rollCount'].append(1)
-    data['rollCount'].append(1)
-    data['rollCount'].append(1)
-    data['dicePick'].append(1)
-    data['dicePick'].append(2)
-    data['dicePick'].append(3)
-    data['dicePick'].append(4)
-    data['dicePick'].append(5)
+    data['rollCount'] = [1,1,1]
+    data['dicePick'] = [1,2,3,4,5]
     print('Turn is over')
-                
-                
-
 #########################Dice Roll###############################
 
 def diceRoll():
@@ -219,8 +322,29 @@ def mouseClick(event):
     if event.x >= 110 and event.x <= 120 and event.y >= 375 and event.y <= 390:
         sixes()
         endTurn()
-    
+    if event.x >= 120 and event.x <= 130 and event.y >= 395 and event.y <= 410 and is3ofakind() == True:
+        is3ofakind()
+        endTurn()
+    if event.x >= 120 and event.x <= 130 and event.y >= 415 and event.y <= 430 and is4ofakind() == True:
+        is4ofakind()
+        endTurn()
+    if event.x >= 120 and event.x <= 130 and event.y >= 435 and event.y <= 450 and isfullhouse() == True:
+        isfullhouse()
+        endTurn()
+    if event.x >= 120 and event.x <= 130 and event.y >= 455 and event.y <= 470 and issmallstraight() == True:
+        issmallstraight()
+        endTurn()
+    if event.x >= 120 and event.x <= 130 and event.y >= 475 and event.y <= 490 and islargestraight() == True:
+        islargestraight()
+        endTurn()
+    if event.x >= 120 and event.x <= 130 and event.y >= 495 and event.y <= 510 and isYahtzee() == True:
+        ischance()
+        endTurn()
+    if event.x >= 120 and event.x <= 130 and event.y >= 515 and event.y <= 530 and isYahtzee() == True:
+        isYahtzee()
+        endTurn()
 ##############################Score - Card #2################################### 
+
     if event.x >= 360 and event.x <= 380 and event.y >= 275 and event.y <= 290:
         ones2()
         endTurn()
@@ -239,7 +363,27 @@ def mouseClick(event):
     if event.x >= 360 and event.x <= 380 and event.y >= 375 and event.y <= 390:
         sixes2()
         endTurn()
-    
+    if event.x >= 370 and event.x <= 390 and event.y >= 395 and event.y <= 410 and is3ofakind2() == True:
+        is3ofakind2()
+        endTurn()
+    if event.x >= 370 and event.x <= 390 and event.y >= 415 and event.y <= 430 and is4ofakind2() == True:
+        is4ofakind2()
+        endTurn()
+    if event.x >= 370 and event.x <= 390 and event.y >= 435 and event.y <= 450 and isfullhouse2() == True:
+        isfullhouse2()
+        endTurn()
+    if event.x >= 370 and event.x <= 390 and event.y >= 455 and event.y <= 470 and issmallstraight2() == True:
+        issmallstraight2()
+        endTurn()
+    if event.x >= 370 and event.x <= 390 and event.y >= 475 and event.y <= 490 and islargestraight2() == True:
+        islargestraight2()
+        endTurn()
+    if event.x >= 370 and event.x <= 390 and event.y >= 495 and event.y <= 510 and ischance2() == True:
+        ischance2()
+        endTurn()  
+    if event.x >= 370 and event.x <= 390 and event.y >= 515 and event.y <= 530 and isYahtzee2() == True:
+        isYahtzee2()
+        endTurn()
 #################################redrawAll################################
 def redrawAll():
     for item in App().spritelist[:]:
@@ -251,7 +395,29 @@ def redrawAll():
     fourScore = TextAsset(str(data['scoreCard'][3][0]) ,fill=black, style='8pt Times')
     fiveScore = TextAsset(str(data['scoreCard'][4][0]) ,fill=black, style='8pt Times')
     sixScore = TextAsset(str(data['scoreCard'][5][0]) ,fill=black, style='8pt Times')
+    is3ofakindScore = TextAsset(str(data['scoreCard'][6][0]) ,fill=black, style='8pt Times')
+    is4ofakindScore = TextAsset(str(data['scoreCard'][7][0]) ,fill=black, style='8pt Times')
+    isfullhouseScore = TextAsset(str(data['scoreCard'][8][0]) ,fill=black, style='8pt Times')
+    issmallstraightScore = TextAsset(str(data['scoreCard'][9][0]) ,fill=black, style='8pt Times')
+    islargestraightScore = TextAsset(str(data['scoreCard'][10][0]) ,fill=black, style='8pt Times')
+    ischanceScore = TextAsset(str(data['scoreCard'][11][0]) ,fill=black, style='8pt Times')
+    isyahtzeeScore = TextAsset(str(data['scoreCard'][12][0]) ,fill=black, style='8pt Times')
     
+    oneScore2 = TextAsset(str(data['scoreCard2'][0][0]) ,fill=black, style='8pt Times')
+    twoScore2 = TextAsset(str(data['scoreCard2'][1][0]) ,fill=black, style='8pt Times')
+    threeScore2 = TextAsset(str(data['scoreCard2'][2][0]) ,fill=black, style='8pt Times')
+    fourScore2 = TextAsset(str(data['scoreCard2'][3][0]) ,fill=black, style='8pt Times')
+    fiveScore2 = TextAsset(str(data['scoreCard2'][4][0]) ,fill=black, style='8pt Times')
+    sixScore2 = TextAsset(str(data['scoreCard2'][5][0]) ,fill=black, style='8pt Times')
+    is3ofakindScore2 = TextAsset(str(data['scoreCard2'][6][0]) ,fill=black, style='8pt Times')
+    is4ofakindScore2 = TextAsset(str(data['scoreCard2'][7][0]) ,fill=black, style='8pt Times')
+    isfullhouseScore2 = TextAsset(str(data['scoreCard2'][8][0]) ,fill=black, style='8pt Times')
+    issmallstraightScore2 = TextAsset(str(data['scoreCard2'][9][0]) ,fill=black, style='8pt Times')
+    islargestraightScore2 = TextAsset(str(data['scoreCard2'][10][0]) ,fill=black, style='8pt Times')
+    ischanceScore2 = TextAsset(str(data['scoreCard2'][11][0]) ,fill=black, style='8pt Times')
+    isyahtzeeScore2 = TextAsset(str(data['scoreCard2'][12][0]) ,fill=black, style='8pt Times')
+    
+    Sprite(greenRectangle,(0,0))
     Sprite(whiteRectangle,(100,100))
     Sprite(whiteRectangle,(200,100))
     Sprite(whiteRectangle,(300,100))
@@ -280,31 +446,47 @@ def redrawAll():
     Sprite(textSix,(60,380))
     Sprite(sixScore,(110,380))
     Sprite(text3ofakind,(60,400))
+    Sprite(is3ofakindScore,(120,400))
     Sprite(text4ofakind,(60,420))
+    Sprite(is4ofakindScore,(120,420))
     Sprite(textfullhouse,(60,440))
+    Sprite(isfullhouseScore,(120,440))
     Sprite(textsmallstraight,(60,460))
+    Sprite(issmallstraightScore,(120,460))
     Sprite(textlargestraight,(60,480))
-    Sprite(textYahtzee,(60,500))
+    Sprite(islargestraightScore,(120,480))
+    Sprite(textchance,(60,500))
+    Sprite(ischanceScore,(120,500))
+    Sprite(textYahtzee,(60,520))
+    Sprite(isyahtzeeScore,(120,520))
     
 ##################Card 2###################
     Sprite(textOne,(310,280))
-    Sprite(oneScore,(360,280))
+    Sprite(oneScore2,(360,280))
     Sprite(textTwo,(310,300))
-    Sprite(twoScore,(360,300))
+    Sprite(twoScore2,(360,300))
     Sprite(textThree,(310,320))
-    Sprite(threeScore,(360,320))
+    Sprite(threeScore2,(360,320))
     Sprite(textFour,(310,340))
-    Sprite(fourScore,(360,340))
+    Sprite(fourScore2,(360,340))
     Sprite(textFive,(310,360))
-    Sprite(fiveScore,(360,360))
+    Sprite(fiveScore2,(360,360))
     Sprite(textSix,(310,380))
-    Sprite(sixScore,(360,380))
+    Sprite(sixScore2,(360,380))
     Sprite(text3ofakind,(310,400))
+    Sprite(is3ofakindScore2,(370,400))
     Sprite(text4ofakind,(310,420))
+    Sprite(is4ofakindScore2,(370,420))
     Sprite(textfullhouse,(310,440))
+    Sprite(isfullhouseScore2,(370,440))
     Sprite(textsmallstraight,(310,460))
+    Sprite(issmallstraightScore2,(370,460))
     Sprite(textlargestraight,(310,480))
-    Sprite(textYahtzee,(310,500))
+    Sprite(islargestraightScore2,(370,480))
+    Sprite(textchance,(310,500))
+    Sprite(ischanceScore2,(370,500))
+    Sprite(textYahtzee,(310,520))
+    Sprite(isyahtzeeScore2,(370,520))
 
     x = 0
     for dice in data['dice']:
@@ -323,15 +505,15 @@ def redrawAll():
         x += 100
 
 
-
 if __name__=='__main__':
     
 
     data = {}
-    data['dice'] = [1,2,3,4,5]
+    data['dice'] = [6,6,6,6,6]
     data['dicePick'] = [1,2,3,4,5]
     data['rollCount'] = [1,1,1]
-    data['scoreCard'] = [[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0]]
+    data['scoreCard'] = [[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0]]
+    data['scoreCard2'] = [[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0]]
     
     redrawAll()
     App().listenMouseEvent("click", mouseClick)
